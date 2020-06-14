6420a9f13f5f9c241782cd035f6b7d9bf0e2c872
Jonas <contato@jonascamargo.com>
2020-06-14 20:20:21 -0300

    estrutura HTML e CSS reset prontos

--- switch/index.php
+++ switch/index.php
@@ -10,9 +10,51 @@
 
 <body>
 
-    <main class="console switch">
-        
-    </main>
+    <div class="console switch">
+        <div class="switch-joycon a">
+            <div class="trigger-left"></div>
+
+            <span class="btn-min"></span>
+
+            <div class="dir-analogico"><i></i></div>
+
+            <div class="dir-digital">
+                <span class="btn-cir top"></span>
+                <span class="btn-cir left"></span>
+                <span class="btn-cir down"></span>
+                <span class="btn-cir right"></span>
+            </div>
+
+            <span class="btn-print"></span>
+        </div>
+        <!-- joycon -->
+
+        <div class="switch-screan">
+            <div class="border">
+                <div class="screan-view"></div>
+            </div>
+        </div>
+        <!-- screan -->
+
+        <div class="switch-joycon b">
+            <div class="trigger-right"></div>
+            
+            <span class="btn-plus"></span>
+
+            <div class="dir-digital">
+                <span class="btn-cir x"></span>
+                <span class="btn-cir a"></span>
+                <span class="btn-cir b"></span>
+                <span class="btn-cir y"></span>
+            </div>
+
+            <div class="dir-analogico"><i></i></div>
+
+            <span class="btn-home"></span>
+        </div>
+        <!-- joycon -->
+    </div>
+    <!-- console -->
     
 </body>
 </html>
